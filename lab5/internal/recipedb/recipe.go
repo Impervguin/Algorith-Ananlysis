203827dc24f421f6e025fa61137eb9c7f08132aa
impervguin <leon53000@yandex.ru>
2024-10-24 12:50:24 +0300

    Rewirte putrecipe func with transaction

--- lab5/internal/recipedb/recipe.go
+++ lab5/internal/recipedb/recipe.go
@@ -21,27 +21,41 @@ INSERT INTO recipes (id, issue_id, url, title, image_url)
 VALUES ($1, $2, $3, $4, $5);
 `
 
-func (p *PgsStorage) PutRecipe(recipe *Recipe) error {
-	_, err := p.conn.Exec(putRecipe, recipe.ID, recipe.IssueID, recipe.Url, recipe.Title, recipe.ImageURL)
-	return err
-}
-
 const insertIngredient = `
 INSERT INTO ingredients (recipe_id, name, amount, unit)
 VALUES ($1, $2, $3, $4);
 `
-
-func (p *PgsStorage) InsertIngredient(recipeID int64, ingredient *Ingredient) error {
-	_, err := p.conn.Exec(insertIngredient, recipeID, ingredient.Name, ingredient.Amount, ingredient.Unit)
-	return err
-}
 
 const insertStep = `
 INSERT INTO steps (recipe_id, step_number, step)
 VALUES ($1, $2, $3);
 `
 
-func (p *PgsStorage) InsertStep(recipeID int64, stepNumber int, step string) error {
-	_, err := p.conn.Exec(insertStep, recipeID, stepNumber, step)
+func (p *PgsStorage) PutRecipe(recipe *Recipe) error {
+	tx, err := p.conn.Begin()
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec(putRecipe, recipe.ID, recipe.IssueID, recipe.Url, recipe.Title, recipe.ImageURL)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	for _, ingredient := range recipe.Ingredients {
+		_, err := tx.Exec(insertIngredient, recipe.ID, ingredient.Name, ingredient.Amount, ingredient.Unit)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+	for i, step := range recipe.Steps {
+		_, err := tx.Exec(insertStep, recipe.ID, i+1, step)
+		if err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+	err = tx.Commit()
 	return err
 }
